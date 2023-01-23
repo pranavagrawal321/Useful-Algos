89a6ee713cb5edba15a9b3ee4b1efbb1ec4034d7
Pranav Agrawal <60977233+pranavagrawal321@users.noreply.github.com>
2023-01-23 23:50:03 +0530

    Update Sieve of Eratosthenes - Prime Numbers upto n.py

--- Sieve of Eratosthenes - Prime Numbers upto n/Sieve of Eratosthenes - Prime Numbers upto n.py
+++ Sieve of Eratosthenes - Prime Numbers upto n/Sieve of Eratosthenes - Prime Numbers upto n.py
@@ -6,7 +6,7 @@ def sieve(n):
     is_prime[0] = is_prime[1] = False
     for i in range(2, math.isqrt(n)):
         if is_prime[i]:
-            for j in range(i*i, n, i):
+            for j in range(i*i, n+1, i):
                 is_prime[j] = False
 
     return list(map(lambda x: x[0], filter(lambda x: x[1], enumerate(is_prime))))
