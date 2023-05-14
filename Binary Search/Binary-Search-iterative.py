33a1ea6e40a1ed823c96cd372702fccddb71248e
Pranav Agrawal <60977233+pranavagrawal321@users.noreply.github.com>
2023-05-15 02:36:32 +0530

    Update Binary-Search-iterative.py
    reduced memory usage

--- Binary Search/Binary-Search-iterative.py
+++ Binary Search/Binary-Search-iterative.py
@@ -2,7 +2,7 @@ def binary_search(arr, element):
     left = 0
     right = len(arr) - 1
     while left <= right:
-        mid = (left + right) // 2
+        mid = left + (right - left) // 2
         if arr[mid] == element:
             return mid
         elif arr[mid] < element:
